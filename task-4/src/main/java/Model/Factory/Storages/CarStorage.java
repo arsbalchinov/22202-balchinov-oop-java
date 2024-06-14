0135285db58f6e981d1641372e3e3f08a4c11a13
Arsalan <arsalanbalchinov1@gmail.com>
2024-06-14 19:03:47 +0700

    Update CarStorage.java

--- task-4/src/main/java/Model/Factory/Storages/CarStorage.java
+++ task-4/src/main/java/Model/Factory/Storages/CarStorage.java
@@ -1,6 +1,7 @@
 package Model.Factory.Storages;
 
 import Model.Factory.FactoryStats;
+import Model.Factory.FactoryController;
 import Model.Factory.Details.Detail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,10 +9,15 @@ import org.slf4j.LoggerFactory;
 public class CarStorage extends Storage<Detail> {
     private static final Logger logger = LoggerFactory.getLogger(CarStorage.class);
     private final boolean log;
+    private FactoryController factoryController;
 
     public CarStorage(int capacity, FactoryStats stats, boolean log) {
         super(capacity, stats);
         this.log = log;
+    }
+
+    public void setFactoryController(FactoryController factoryController) {
+        this.factoryController = factoryController;
     }
 
     @Override
@@ -24,7 +30,10 @@ public class CarStorage extends Storage<Detail> {
     }
 
     @Override
-    public synchronized Detail get() {
+    public Detail get() {
+        synchronized (factoryController) {
+            factoryController.notify();
+        }
         return super.get();
     }
 }
